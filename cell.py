1eab1c7d00d094e209cf1ace2f3b26670da2b895
Jorge Guedes <jorgeguedes16@gmail.com>
2024-02-19 16:10:36 +0000

    Implement validation for marking cells in the Cell class and add a to_dict method for converting cells to dictionary representation

--- cell.py
+++ cell.py
@@ -1,11 +1,23 @@
 class Cell:
     def __init__(self, x, y):
-        self.marker = " "
         self.x = x
         self.y = y
+        self.marker = " "
     
     def is_empty(self):
         return self.marker == " "
 
     def mark(self, marker):
-        self.marker = marker
+        if (marker.upper() != "X" and marker.upper() != "O"):
+            raise Exception("Invalid marker!")
+        elif self.marker != " ":
+            raise Exception("Choose another cell!")
+    
+        self.marker = marker.upper()
+
+    def to_dict(self):
+        return {
+            "x": self.x,
+            "y": self.y,
+            "marker": self.marker
+        }
