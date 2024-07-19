64eabc2092ea10493d81e3dc8dfb5777a06acc85
Jorge Guedes <jorgeguedes16@gmail.com>
2024-07-19 07:16:57 +0100

    Added integration tests to verify game winner

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -22,3 +22,14 @@ class TestApp(unittest.TestCase):
     def test_current_player(self):
         response = self.app.get('/player/current')
         self.assertEqual(response.json, {'currentPlayer': 'X'})
+
+    def test_check_winner(self):
+        self.app.post('/cell/mark', data={'x': 2, 'y': 0, 'mark': 'X'})
+        self.app.post('/cell/mark', data={'x': 2, 'y': 1, 'mark': 'X'})
+        self.app.post('/cell/mark', data={'x': 2, 'y': 2, 'mark': 'X'})
+        response = self.app.get('/check_winner')
+        self.assertEqual(response.json, {
+            "win_cell": {"marker": "X", "x": 2, "y": 0},
+            "win_cell2": {"marker": "X", "x": 2, "y": 1},
+            "win_cell3": {"marker": "X", "x": 2, "y": 2}
+        })
