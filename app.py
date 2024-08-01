e1e089765d81699ab6cb6c03f5bf1c102483f63c
Jorge Guedes <jorgeguedes16@gmail.com>
2024-08-01 09:35:27 +0100

    Added multi-game support, create new games and manage game state with IDs

--- app.py
+++ app.py
@@ -9,48 +9,69 @@ from tictactoe.board import Board
 
 app = Flask(__name__)
 
-board = Board()
-currentPlayer = "X"
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.currentPlayer = "X"
 
+games = []
+
+def get_game(id):
+    """Retrieve the game with the specified ID"""
+    if 0 <= id < len(games):
+        return games[id]
+    
 @app.route("/")
 def welcome():
     """Render the welcome page"""
     return render_template("index.html")
 
-@app.route("/cell/<int:x_coord>/<int:y_coord>")
-def get_cell(x_coord, y_coord):
+@app.route("/new_game", methods=["POST"])
+def new_game():
+    global games
+    """Add new game to the pool and return its ID"""
+    games.append(Game())
+    return {"id": len(games) - 1}
+
+@app.route("/game/<int:id>/cell/<int:x_coord>/<int:y_coord>")
+def get_cell(id, x_coord, y_coord):
     """Get the state of a specific cell"""
-    return board.get_cell(x_coord, y_coord).to_dict()
+    game = get_game(id)
+    return game.board.get_cell(x_coord, y_coord).to_dict()
 
-@app.route("/board")
-def get_board():
+@app.route("/game/<int:id>/board")
+def get_board(id):
     """Get the current state of the board"""
-    return {"grid": board.to_dict()}
+    game = get_game(id)
+    return {"grid": game.board.to_dict()}
 
-@app.route('/player/current', methods=['GET', 'POST'])
-def current_player():
+@app.route('/game/<int:id>/player/current', methods=['GET', 'POST'])
+def current_player(id):
     """Get or set the current player"""
-    global currentPlayer
+    game = get_game(id)
     if request.method == 'POST':
-        currentPlayer = request.form['currentPlayer']
+        game.currentPlayer = request.form["currentPlayer"]
         return {"success": True}
-    return {"currentPlayer": currentPlayer}
+    else:
+        return {"currentPlayer": game.currentPlayer}
 
-@app.route("/cell/mark", methods=["POST"])
-def post_cell_mark():
+@app.route("/game/<int:id>/cell/mark", methods=["POST"])
+def post_cell_mark(id):
     """Mark a cell on the board"""
     try:
         x_coord = int(request.form["x"])
         y_coord = int(request.form["y"])
         mark = request.form["mark"]
-
         if not (0 <= x_coord <= 2 and 0 <= y_coord <= 2):
             raise ValueError("Invalid input. Please enter a valid number (0-2).")
 
-        cell = board.get_cell(x_coord, y_coord)
+        game = get_game(id)
+        cell = game.board.get_cell(x_coord, y_coord)
+        if cell.marker != " ":
+            return {"error": "Choose another cell!"}
+
         cell.mark(mark)
-        global current_player
-        current_player = "O" if current_player == "X" else "X"
+        game.currentPlayer = "O" if game.currentPlayer == "X" else "X"
         return cell.to_dict()
 
     except ValueError:
@@ -58,24 +79,24 @@ def post_cell_mark():
     except Exception as error:
         return {"error": str(error)}
 
-@app.route("/check_winner", methods=["GET"])
-def check_winner():
+@app.route("/game/<int:id>/check_winner", methods=["GET"])
+def check_winner(id):
     """Check if there is a winner on the board"""
-    win_cell, win_cell2, win_cell3 = board.check_winner()
-
+    game = get_game(id)
+    win_cell, win_cell2, win_cell3 = game.board.check_winner()
     if win_cell:
         return {"win_cell": win_cell, "win_cell2": win_cell2, "win_cell3": win_cell3}
-
-    if board.is_board_full():
+    elif game.board.is_board_full():
         return {"winner": "Tie"}
+    else:
+        return {"winner": None}
 
-    return {"winner": None}
-
-@app.route("/reset_board", methods=["GET"])
-def reset_board():
-    """Reset the board to its initial state"""
-    board.reset()
-    return "Ok"
+@app.route("/game/<int:id>/reset_board", methods=["POST"])
+def reset_board(id):
+    """Reset the board for the specified game"""
+    game = get_game(id)
+    game.board.reset()
+    return {"success": True}
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)  # mac port 8000, server 5000
+    app.run(host='0.0.0.0', port=5000, debug=True) # mac port 8000, server 5000
