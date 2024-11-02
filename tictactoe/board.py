57beff178f339c9f1447643fb67bce807dbce450
Jorge Guedes <jorgeguedes16@gmail.com>
2024-11-02 18:27:30 +0000

    Updated variable names to conform to snake_case

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -16,31 +16,31 @@ class Board:
 
     def create_board(self):
         """Create a 3x3 board by initializing Cell objects for each position."""
-        for x in range(3):
-            for y in range(3):
-                self.grid.append(Cell(x, y))
+        for x_coord in range(3):
+            for y_coord in range(3):
+                self.grid.append(Cell(x_coord, y_coord))
 
     def show_board(self):
         """Display the current state of the board in the console."""
-        for y in range(3):
-            print("|" + self.get_cell(0, y).marker + "|" +
-                  self.get_cell(1, y).marker + "|" +
-                  self.get_cell(2, y).marker + "|")
+        for y_coord in range(3):
+            print("|" + self.get_cell(0, y_coord).marker + "|" +
+                  self.get_cell(1, y_coord).marker + "|" +
+                  self.get_cell(2, y_coord).marker + "|")
 
-    def get_cell(self, x, y):
+    def get_cell(self, x_coord, y_coord):
         """Return the cell at the specified coordinates."""
         for cell in self.grid:
-            if cell.x == x and cell.y == y:
+            if cell.x == x_coord and cell.y == y_coord:
                 return cell
         return None
 
-    def get_mark(self, x, y):
+    def get_mark(self, x_coord, y_coord):
         """Return the marker of the cell at the specified coordinates."""
-        return self.get_cell(x, y).marker
+        return self.get_cell(x_coord, y_coord).marker
 
-    def play(self, x, y, marker):
+    def play(self, x_coord, y_coord, marker):
         """Place a marker on the board if it's a valid move."""
-        cell = self.get_cell(x, y)
+        cell = self.get_cell(x_coord, y_coord)
 
         if marker == "X" or marker == "O":
             cell.mark(marker)
@@ -52,22 +52,22 @@ class Board:
     def check_winner(self):
         """Check for a winning combination on the board."""
         # Vertical check
-        for x in range(3):
-            if (self.get_mark(x, 0) == self.get_mark(x, 1) ==
-                self.get_mark(x, 2) != " "):
-                print("\nPlayer " + self.get_mark(x, 0) + " is the winner!")
-                return (self.get_cell(x, 0).to_dict(),
-                        self.get_cell(x, 1).to_dict(),
-                        self.get_cell(x, 2).to_dict())
+        for x_coord in range(3):
+            if (self.get_mark(x_coord, 0) == self.get_mark(x_coord, 1) ==
+                self.get_mark(x_coord, 2) != " "):
+                print("\nPlayer " + self.get_mark(x_coord, 0) + " is the winner!")
+                return (self.get_cell(x_coord, 0).to_dict(),
+                        self.get_cell(x_coord, 1).to_dict(),
+                        self.get_cell(x_coord, 2).to_dict())
 
         # Horizontal check
-        for y in range(3):
-            if (self.get_mark(0, y) == self.get_mark(1, y) ==
-                self.get_mark(2, y) != " "):
-                print("\nPlayer " + self.get_mark(0, y) + " is the winner!")
-                return (self.get_cell(0, y).to_dict(),
-                        self.get_cell(1, y).to_dict(),
-                        self.get_cell(2, y).to_dict())
+        for y_coord in range(3):
+            if (self.get_mark(0, y_coord) == self.get_mark(1, y_coord) ==
+                self.get_mark(2, y_coord) != " "):
+                print("\nPlayer " + self.get_mark(0, y_coord) + " is the winner!")
+                return (self.get_cell(0, y_coord).to_dict(),
+                        self.get_cell(1, y_coord).to_dict(),
+                        self.get_cell(2, y_coord).to_dict())
 
         # Diagonal check
         if (self.get_mark(0, 0) == self.get_mark(1, 1) ==
